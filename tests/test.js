46081bdf2ff6ce9b42ff98d858c45571b5fe70ff
Rakesh <rbandi211570@gmail.com>
2022-09-15 15:46:14 -0700

    blah

--- tests/test.js
+++ tests/test.js
@@ -6,36 +6,37 @@ const { Builder, By, until } = require('selenium-webdriver');
 
 console.log('2 is: ' + 2);
 (async function example() {
-    let driver;
     try {
-        driver = await new Builder().forBrowser('firefox').build();
-        let testNum = 1;
-        console.log('testNum is: ' + testNum);
-        /*await driver.get('http://localhost:3000/');
-        let elem = await driver.findElement(By.id('testConsoleStatus'));
-        await driver.wait(until.elementTextContains(elem, 'NEXT'));
-
-        while (true) {
-            await driver.wait(until.elementTextContains(elem, parseInt(testNum, 10) + 1));
-            let statusText = await elem.getText();
-            if (/.*FAIL..test(statusText)) {
-                let errText = await driver.findElement(By.id('testConsoleError')).getText();
-                console.log('\n' + errText);
-                process.exitCode = 1;
-                break;
-            } else if (/.*SUCCESS..test(statusText)) {
-                console.log('ALL TESTS PASSED SUCCESSFULLY');
-                break;
-            }
-            let logText = await driver.findElement(By.id('testConsoleLog')).getText();
-            let logTextArr = logText.split(',');
-            console.log(logTextArr[logTextArr.length - 1]);
-            testNum++;
-        }*/
+        let driver = await new Builder().forBrowser('firefox').build();
+        try {
+            let testNum = 1;
+            console.log('testNum is: ' + testNum);
+            /*await driver.get('http://localhost:3000/');
+            let elem = await driver.findElement(By.id('testConsoleStatus'));
+            await driver.wait(until.elementTextContains(elem, 'NEXT'));
+    
+            while (true) {
+                await driver.wait(until.elementTextContains(elem, parseInt(testNum, 10) + 1));
+                let statusText = await elem.getText();
+                if (/.*FAIL..test(statusText)) {
+                    let errText = await driver.findElement(By.id('testConsoleError')).getText();
+                    console.log('\n' + errText);
+                    process.exitCode = 1;
+                    break;
+                } else if (/.*SUCCESS..test(statusText)) {
+                    console.log('ALL TESTS PASSED SUCCESSFULLY');
+                    break;
+                }
+                let logText = await driver.findElement(By.id('testConsoleLog')).getText();
+                let logTextArr = logText.split(',');
+                console.log(logTextArr[logTextArr.length - 1]);
+                testNum++;
+            }*/
+        } finally {
+            await driver.quit();
+            process.exit();
+        }
     } catch (e) {
         console.log('myError: ' + e);
-    } finally {
-        await driver.quit();
-        process.exit();
     }
 })();
