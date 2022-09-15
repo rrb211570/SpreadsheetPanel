050f7415f7ab519f526c8fe746bcb31fbc3aaac9
Rakesh <rbandi211570@gmail.com>
2022-09-15 16:36:13 -0700

    blah

--- tests/test.js
+++ tests/test.js
@@ -17,19 +17,19 @@ console.log('2 is: ' + 2);
         try {
             let testNum = 1;
             console.log('testNum is: ' + testNum);
-            /*await driver.get('http://localhost:3000/');
+            await driver.get('http://localhost:3000/');
             let elem = await driver.findElement(By.id('testConsoleStatus'));
             await driver.wait(until.elementTextContains(elem, 'NEXT'));
     
             while (true) {
                 await driver.wait(until.elementTextContains(elem, parseInt(testNum, 10) + 1));
                 let statusText = await elem.getText();
-                if (/.*FAIL..test(statusText)) {
+                if (/.*FAIL.*/.test(statusText)) {
                     let errText = await driver.findElement(By.id('testConsoleError')).getText();
                     console.log('\n' + errText);
                     process.exitCode = 1;
                     break;
-                } else if (/.*SUCCESS..test(statusText)) {
+                } else if (/.*SUCCESS.*/.test(statusText)) {
                     console.log('ALL TESTS PASSED SUCCESSFULLY');
                     break;
                 }
@@ -37,7 +37,7 @@ console.log('2 is: ' + 2);
                 let logTextArr = logText.split(',');
                 console.log(logTextArr[logTextArr.length - 1]);
                 testNum++;
-            }*/
+            }
         } finally {
             await driver.quit();
             process.exit();
