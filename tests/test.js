3fa0bcfc48b75e5f14c48262866f247dad32b275
Rakesh <rbandi211570@gmail.com>
2022-09-18 20:17:20 -0700

    blah

--- tests/test.js
+++ tests/test.js
@@ -10,35 +10,33 @@ myOptions.addArguments("--headless");
             .forBrowser('firefox')
             .setFirefoxOptions(myOptions)
             .build();
-        try {
-            await driver.get('http://localhost:3000/');
-            let elem = await driver.findElement(By.id('testConsoleStatus'));
-            await driver.wait(until.elementTextContains(elem, 'NEXT'));
-    
-            while (true) {
-                await driver.wait(until.elementTextContains(elem, parseInt(testNum, 10) + 1));
-                let statusText = await elem.getText();
-                if (/.*FAIL.*/.test(statusText)) {
-                    let errText = await driver.findElement(By.id('testConsoleError')).getText();
-                    throw errText;
-                } else if (/.*SUCCESS.*/.test(statusText)) {
-                    console.log('ALL TESTS PASSED SUCCESSFULLY');
-                    throw 'blah';
-                    break;
-                }
-                let logText = await driver.findElement(By.id('testConsoleLog')).getText();
-                let logTextArr = logText.split(',');
-                console.log(logTextArr[logTextArr.length - 1]);
-                testNum++;
+        await driver.get('http://localhost:3000/');
+        let elem = await driver.findElement(By.id('testConsoleStatus'));
+        await driver.wait(until.elementTextContains(elem, 'NEXT'));
+
+        while (true) {
+            await driver.wait(until.elementTextContains(elem, parseInt(testNum, 10) + 1));
+            let statusText = await elem.getText();
+            if (/.*FAIL.*/.test(statusText)) {
+                let errText = await driver.findElement(By.id('testConsoleError')).getText();
+                throw errText;
+            } else if (/.*SUCCESS.*/.test(statusText)) {
+                console.log('ALL TESTS PASSED SUCCESSFULLY');
+                throw 'blah';
+                // break;
             }
-        } finally {
-            core.setOutput("testingSuccess", false);
-            await driver.quit();
-            process.exit();
+            let logText = await driver.findElement(By.id('testConsoleLog')).getText();
+            let logTextArr = logText.split(',');
+            console.log(logTextArr[logTextArr.length - 1]);
+            testNum++;
         }
     } catch (e) {
         core.setOutput("testingSuccess", false);
-        core.setFailed(errMsg);
+        core.setFailed(e);
         process.exit(1);
+    } finally {
+        core.setOutput("testingSuccess", true);
+        await driver.quit();
+        process.exit();
     }
 })();
